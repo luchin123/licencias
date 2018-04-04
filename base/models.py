dfe8bef66d05272e829e7d958869590a54b8b4b8
Luis Bueno <lbueno1889@gmail.com>
2018-04-04 16:24:19 -0500

    agregar nueva clasee sancion y gitnore agrego nuevas exclusiones

--- base/models.py
+++ base/models.py
@@ -4,30 +4,41 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Persona(models.Model):
-	nombres = models.CharField(max_length=255)
-	apellidos = models.CharField(max_length=255)
-	dni = models.CharField(max_length=8)
-	fecha_nacimiento = models.DateField()
-	direccion = models.CharField(max_length=255)
-	donacion = models.BooleanField(default=True)
-	foto = models.ImageField(upload_to='fotos', max_length=100)
-	firma = models.ImageField(upload_to='firmas', max_length=100)
+    nombres = models.CharField(max_length=255)
+    apellidos = models.CharField(max_length=255)
+    dni = models.CharField(max_length=8)
+    fecha_nacimiento = models.DateField()
+    direccion = models.CharField(max_length=255)
+    donacion = models.BooleanField(default=True)
+    foto = models.ImageField(upload_to='fotos', max_length=100)
+    firma = models.ImageField(upload_to='firmas', max_length=100)
 
 class Autoridad(models.Model):
-	nombre_autoridad = models.CharField(max_length=255)
-	fechainicio_autoridad = models.DateField()
-	fechainicio_autoridad = models.DateField()
-	firma_autoridad = models.ImageField(upload_to='firmas_autoridad', max_length=100)
+    nombre_autoridad = models.CharField(max_length=255)
+    fechainicio_autoridad = models.DateField()
+    fechainicio_autoridad = models.DateField()
+    firma_autoridad = models.ImageField(upload_to='firmas_autoridad', max_length=100)
 
-	class Meta:
-		verbose_name_plural='Autoridades'
+    class Meta:
+        verbose_name_plural='Autoridades'
 
 class Licencia(models.Model):
-	persona = models.ForeignKey(Persona)
-	autoridad = models.ForeignKey(Autoridad)
-	numero_licencias = models.CharField(max_length=6)
-	fecha_expedicion = models.DateField()
-	fecha_revalidacion = models.DateField()
-	restricciones = models.CharField(max_length=6)
+    persona = models.ForeignKey(Persona)
+    autoridad = models.ForeignKey(Autoridad)
+    numero_licencias = models.CharField(max_length=6)
+    fecha_expedicion = models.DateField()
+    fecha_revalidacion = models.DateField()
+    restricciones = models.CharField(max_length=6)
 
+class Sancion(models.Model):
+    persona = models.ForeignKey(Persona)
+    licencia = models.ForeignKey(Licencia)
+    fecha_infracion = models.DateField()
+    numero_papeleta = models.IntegerField()
+    entidad = models.CharField(max_length=255)
+    distrito = models.CharField(max_length=255)
+    infracion = models.CharField(max_length=255)
+    gradoAlcohol = models.CharField(max_length=255)
+    retencion = models.BooleanField(default=True)
+    carnet_policial = models.IntegerField()
 
