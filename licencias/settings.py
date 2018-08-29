c819e4483475b452f896127e7525619ab7cab0d4
Luis Bueno <lbueno1889@gmail.com>
2018-08-28 23:21:44 -0500

    SEttings mysql

--- licencias/settings.py
+++ licencias/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'licencias.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASS'),
+        'HOST': config('DBHOST')
     }
 }
 
